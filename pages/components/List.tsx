c6293ba8d553a61851cd477db58bf360f1538065
eminvergil <emintrhack@gmail.com>
2022-07-03 19:53:40 +0300

    add key

--- pages/components/List.tsx
+++ pages/components/List.tsx
@@ -5,8 +5,11 @@ export default function List() {
   const { albums } = useContext(AlbumContext);
   return (
     <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-10  p-5 justify-center items-center mx-auto">
-      {albums.map((album: any) => (
-        <div className="flex flex-col gap-10  p-5 justify-center items-center mx-auto shadow-sm hover:shadow-2xl transition-shadow duration-500 ease-in-out">
+      {albums.map((album: any, key: any) => (
+        <div
+          key={key}
+          className="flex flex-col gap-10  p-5 justify-center items-center mx-auto shadow-sm hover:shadow-2xl transition-shadow duration-500 ease-in-out"
+        >
           <img src={album.images[0].url} alt="album" />
           <h3>{album.name}</h3>
           <p>{album.artists[0].name}</p>
